9d1a8cc17f5239ac440a9aba84ca5228946dbc9d
Daan Meijer <daansander02@outlook.com>
2022-01-17 08:18:44 +0100

    Norminize mini_utils

--- mini_utils/include/mini_assert.h
+++ mini_utils/include/mini_assert.h
@@ -1,9 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   mini_assert.h                                      :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: dmeijer <dmeijer@student.codam.nl>           +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2022/01/17 08:17:37 by dmeijer       #+#    #+#                 */
+/*   Updated: 2022/01/17 08:17:47 by dmeijer       ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef MINI_ASSERT_H
 # define MINI_ASSERT_H
 
 # include <ft_stdbool.h>
 
-void mini_assert(ft_bool assertion);
+void	mini_assert(ft_bool assertion);
 
 #endif
-
--- mini_utils/src/mini_assert.c
+++ mini_utils/src/mini_assert.c
@@ -1,8 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   mini_assert.c                                      :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: dmeijer <dmeijer@student.codam.nl>           +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2022/01/17 08:18:00 by dmeijer       #+#    #+#                 */
+/*   Updated: 2022/01/17 08:18:08 by dmeijer       ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "mini_assert.h"
 #include <stdlib.h>
 
-void mini_assert(ft_bool assertion) {
+void
+	mini_assert(ft_bool assertion)
+{
 	if (!assertion)
 		exit(EXIT_FAILURE);
 }
-
